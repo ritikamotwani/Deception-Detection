040bd0a8622dcabbd0eafd3de2dd5717fff74c68
ritika <ritika.motwani0712@gmail.com>
2019-02-21 14:21:16 +0800

    Add parameters to Naives Bayes

---
+++ Naive_Bayes.py
@@ -0,0 +1,116 @@
+# gensim modules
+import gensim
+from gensim import utils
+from gensim import corpora,models
+# numpy
+import numpy
+# classifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+import os
+from random import shuffle
+import pandas
+from sklearn import model_selection, preprocessing, naive_bayes
+import string
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import GaussianNB
+
+
+#create source
+sources = []
+labels = []
+
+trainingPath = './dataset/TrainingSet/'
+for home, dirs, files in os.walk(trainingPath+'Deceptive'):
+    for filename in files:
+        sources.append(home+'/'+filename)
+        labels.append(1)
+
+for home, dirs, files in os.walk(trainingPath+'Truthful'):
+    for filename in files:
+        sources.append(home + '/' + filename)
+        labels.append(0)
+
+testPath = './dataset/TestingSet/'
+for home, dirs, files in os.walk(testPath + 'Deceptive'):
+    for filename in files:
+        sources.append(home + '/' + filename)
+        labels.append(1)
+
+for home, dirs, files in os.walk(testPath + 'Truthful'):
+    for filename in files:
+        sources.append(home + '/' + filename)
+        labels.append(0)
+text = []
+for source in sources:
+    with open(source) as f_input:
+        text.append(f_input.read())
+
+# create a dataframe using texts and lables
+trainDF = pandas.DataFrame()
+trainDF['text'] = text
+trainDF['label'] = labels
+
+# split the dataset into training and validation datasets 
+train_x, valid_x, train_y, valid_y = model_selection.train_test_split(trainDF['text'], trainDF['label'], test_size=0.25, train_size=0.75, shuffle=False)
+
+# label encode the target variable 
+encoder = preprocessing.LabelEncoder()
+train_y = encoder.fit_transform(train_y)
+valid_y = encoder.fit_transform(valid_y)
+
+# create a count vectorizer object 
+count_vect = CountVectorizer(analyzer='word', token_pattern=r'\w{1,}')
+count_vect.fit(trainDF['text'])
+
+# transform the training and validation data using count vectorizer object
+xtrain_count =  count_vect.transform(train_x)
+xvalid_count =  count_vect.transform(valid_x)
+
+# word level tf-idf
+tfidf_vect = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', max_features=5000)
+tfidf_vect.fit(trainDF['text'])
+xtrain_tfidf =  tfidf_vect.transform(train_x)
+xvalid_tfidf =  tfidf_vect.transform(valid_x)
+
+# ngram level tf-idf 
+tfidf_vect_ngram = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', ngram_range=(2,3), max_features=5000)
+tfidf_vect_ngram.fit(trainDF['text'])
+xtrain_tfidf_ngram =  tfidf_vect_ngram.transform(train_x)
+xvalid_tfidf_ngram =  tfidf_vect_ngram.transform(valid_x)
+
+# characters level tf-idf
+tfidf_vect_ngram_chars = TfidfVectorizer(analyzer='char', token_pattern=r'\w{1,}', ngram_range=(2,3), max_features=5000)
+tfidf_vect_ngram_chars.fit(trainDF['text'])
+xtrain_tfidf_ngram_chars =  tfidf_vect_ngram_chars.transform(train_x) 
+xvalid_tfidf_ngram_chars =  tfidf_vect_ngram_chars.transform(valid_x) 
+
+
+def train_model(classifier, feature_vector_train, label, feature_vector_valid, is_neural_net=False):
+    # fit the training dataset on the classifier
+    classifier.fit(feature_vector_train, label)
+    
+    # predict the labels on validation dataset
+    predictions = classifier.predict(feature_vector_valid)
+    
+    if is_neural_net:
+        predictions = predictions.argmax(axis=-1)
+    
+    return accuracy_score(predictions, valid_y)
+
+# Naive Bayes on Count Vectors
+accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_count, train_y, xvalid_count)
+print("NB, Count Vectors: ", accuracy)
+
+# Naive Bayes on Word Level TF IDF Vectors
+accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_tfidf, train_y, xvalid_tfidf)
+print("NB, WordLevel TF-IDF: ", accuracy)
+
+# Naive Bayes on Ngram Level TF IDF Vectors
+accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_tfidf_ngram, train_y, xvalid_tfidf_ngram)
+print("NB, N-Gram Vectors: ", accuracy)
+
+# Naive Bayes on Character Level TF IDF Vectors
+accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_tfidf_ngram_chars, train_y, xvalid_tfidf_ngram_chars)
+print("NB, CharLevel Vectors: ", accuracy)
