f6257b92d6823dd895ae7194154dd99e097afd6c
ritika <ritika.motwani0712@gmail.com>
2019-02-21 14:20:41 +0800

    SVM with ngrams

--- SVMandNgrams.py
+++ SVMandNgrams.py
@@ -9,11 +9,12 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 import os
 from random import shuffle
 import pandas
-from sklearn import model_selection, preprocessing
+from sklearn import model_selection, preprocessing, naive_bayes
 import string
 from sklearn.decomposition import LatentDirichletAllocation
 from sklearn import svm
 from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import GaussianNB
 
 
 #create source
@@ -52,7 +53,7 @@ trainDF['text'] = text
 trainDF['label'] = labels
 
 # split the dataset into training and validation datasets 
-train_x, valid_x, train_y, valid_y = model_selection.train_test_split(trainDF['text'], trainDF['label'], test_size=0.20)
+train_x, valid_x, train_y, valid_y = model_selection.train_test_split(trainDF['text'], trainDF['label'], test_size=0.25, train_size=0.75, shuffle=False)
 
 # label encode the target variable 
 encoder = preprocessing.LabelEncoder()
@@ -74,51 +75,17 @@ xtrain_tfidf =  tfidf_vect.transform(train_x)
 xvalid_tfidf =  tfidf_vect.transform(valid_x)
 
 # ngram level tf-idf 
-tfidf_vect_ngram = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', ngram_range=(2,3), max_features=500)
+tfidf_vect_ngram = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', ngram_range=(2,3), max_features=5000)
 tfidf_vect_ngram.fit(trainDF['text'])
 xtrain_tfidf_ngram =  tfidf_vect_ngram.transform(train_x)
 xvalid_tfidf_ngram =  tfidf_vect_ngram.transform(valid_x)
 
 # characters level tf-idf
-tfidf_vect_ngram_chars = TfidfVectorizer(analyzer='char', token_pattern=r'\w{1,}', ngram_range=(1,2), max_features=5000)
+tfidf_vect_ngram_chars = TfidfVectorizer(analyzer='char', token_pattern=r'\w{1,}', ngram_range=(2,3), max_features=5000)
 tfidf_vect_ngram_chars.fit(trainDF['text'])
 xtrain_tfidf_ngram_chars =  tfidf_vect_ngram_chars.transform(train_x) 
 xvalid_tfidf_ngram_chars =  tfidf_vect_ngram_chars.transform(valid_x) 
 
-# NLP based features
-trainDF['char_count'] = trainDF['text'].apply(len)
-trainDF['word_count'] = trainDF['text'].apply(lambda x: len(x.split()))
-trainDF['word_density'] = trainDF['char_count'] / (trainDF['word_count']+1)
-trainDF['punctuation_count'] = trainDF['text'].apply(lambda x: len("".join(_ for _ in x if _ in string.punctuation))) 
-trainDF['title_word_count'] = trainDF['text'].apply(lambda x: len([wrd for wrd in x.split() if wrd.istitle()]))
-trainDF['upper_case_word_count'] = trainDF['text'].apply(lambda x: len([wrd for wrd in x.split() if wrd.isupper()]))
-
-pos_family = {
-    'noun' : ['NN','NNS','NNP','NNPS'],
-    'pron' : ['PRP','PRP$','WP','WP$'],
-    'verb' : ['VB','VBD','VBG','VBN','VBP','VBZ'],
-    'adj' :  ['JJ','JJR','JJS'],
-    'adv' : ['RB','RBR','RBS','WRB']
-}
-
-# function to check and get the part of speech tag count of a words in a given sentence
-def check_pos_tag(x, flag):
-    cnt = 0
-    try:
-        wiki = textblob.TextBlob(x)
-        for tup in wiki.tags:
-            ppo = list(tup)[1]
-            if ppo in pos_family[flag]:
-                cnt += 1
-    except:
-        pass
-    return cnt
-
-trainDF['noun_count'] = trainDF['text'].apply(lambda x: check_pos_tag(x, 'noun'))
-trainDF['verb_count'] = trainDF['text'].apply(lambda x: check_pos_tag(x, 'verb'))
-trainDF['adj_count'] = trainDF['text'].apply(lambda x: check_pos_tag(x, 'adj'))
-trainDF['adv_count'] = trainDF['text'].apply(lambda x: check_pos_tag(x, 'adv'))
-trainDF['pron_count'] = trainDF['text'].apply(lambda x: check_pos_tag(x, 'pron'))
 
 def train_model(classifier, feature_vector_train, label, feature_vector_valid, is_neural_net=False):
     # fit the training dataset on the classifier
