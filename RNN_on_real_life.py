0f2b463d9745ac877fb2f88dcdaa59f54d1b1145
ritikamotwani <ritika.motwani0712@gmail.com>
2019-02-28 17:04:07 +0800

    RNN on real life dataset

---
+++ RNN_on_real_life.py
@@ -0,0 +1,106 @@
+# gensim modules
+import gensim
+from gensim import utils
+from gensim import corpora,models
+# numpy
+import numpy
+# classifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+import os
+from random import shuffle
+import pandas
+from sklearn import model_selection, preprocessing, naive_bayes
+import string
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn import svm
+from keras.models import Model
+from sklearn.metrics import accuracy_score
+from keras.optimizers import RMSprop
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing import sequence
+from keras.utils import to_categorical
+from keras.callbacks import EarlyStopping
+from keras.layers import LSTM, Activation, Dense, Dropout, Input, Embedding
+
+
+#create source
+sources = []
+labels = []
+
+trainingPath = './dataset/TrainingSet/'
+for home, dirs, files in os.walk(trainingPath+'Deceptive'):
+    for filename in files:
+        sources.append(home+'/'+filename)
+        labels.append(1)
+
+for home, dirs, files in os.walk(trainingPath+'Truthful'):
+    for filename in files:
+        sources.append(home + '/' + filename)
+        labels.append(0)
+
+testPath = './dataset/TestingSet/'
+for home, dirs, files in os.walk(testPath + 'Deceptive'):
+    for filename in files:
+        sources.append(home + '/' + filename)
+        labels.append(1)
+
+for home, dirs, files in os.walk(testPath + 'Truthful'):
+    for filename in files:
+        sources.append(home + '/' + filename)
+        labels.append(0)
+
+text = []
+for source in sources:
+    with open(source) as f_input:
+        text.append(f_input.read())
+
+# create a dataframe using texts and lables
+trainDF = pandas.DataFrame()
+trainDF['text'] = text
+trainDF['label'] = labels
+
+# split the dataset into training and validation datasets 
+X_train,X_test,Y_train,Y_test = model_selection.train_test_split(trainDF['text'], trainDF['label'], test_size=0.15)
+
+
+#Tokenize the data and convert the text to sequences.
+#Add padding to ensure that all the sequences have the same shape.
+#There are many ways of taking the max_len and here an arbitrary length of 150 is chosen.
+max_words = 1000
+max_len = 150
+tok = Tokenizer(num_words=max_words)
+tok.fit_on_texts(X_train)
+sequences = tok.texts_to_sequences(X_train)
+sequences_matrix = sequence.pad_sequences(sequences,maxlen=max_len)
+
+# Define the RNN Structure
+def RNN():
+    inputs = Input(name='inputs',shape=[max_len])
+    layer = Embedding(max_words,50,input_length=max_len)(inputs)
+    layer = LSTM(64)(layer)
+    layer = Dense(256,name='FC1')(layer)
+    layer = Activation('relu')(layer)
+    layer = Dropout(0.5)(layer)
+    layer = Dense(1,name='out_layer')(layer)
+    layer = Activation('sigmoid')(layer)
+    model = Model(inputs=inputs,outputs=layer)
+    return model
+
+model = RNN()
+model.summary()
+model.compile(loss='binary_crossentropy',optimizer=RMSprop(),metrics=['accuracy'])
+
+
+
+
+#Fit on the training data.
+model.fit(sequences_matrix,Y_train,batch_size=128,epochs=100,
+          validation_split=0.2,callbacks=[EarlyStopping(monitor='val_loss',min_delta=0.0000001)])
+
+#Process the test data
+test_sequences = tok.texts_to_sequences(X_test)
+test_sequences_matrix = sequence.pad_sequences(test_sequences,maxlen=max_len)
+
+#Evaluate the test set
+accr = model.evaluate(test_sequences_matrix,Y_test)
+print('Test set\n  Loss: {:0.3f}\n  Accuracy: {:0.3f}'.format(accr[0],accr[1]))
