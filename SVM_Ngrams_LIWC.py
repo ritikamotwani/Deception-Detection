cde0ebf90f3debf9024b74d8add200d240b33eee
aiman9 <aimanshariff.97@gmail.com>
2019-03-13 15:38:50 +0800

    Include K-fold cross validation for model

--- SVM_Ngrams_LIWC.py
+++ SVM_Ngrams_LIWC.py
@@ -8,16 +8,18 @@ from sklearn import model_selection, preprocessing, naive_bayes
 from sklearn.decomposition import LatentDirichletAllocation
 from sklearn import svm
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
 from sklearn.naive_bayes import GaussianNB
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_validate
 from sklearn.model_selection import KFold
 from scipy.sparse import csr_matrix
+from statistics import mean
 
 sources = [[] for i in range(4)]
 
 # for infile in glob.glob(datapath+'deceptive_neg/*.txt'):
 
-datapath='./dataset1/'
+datapath='./Spam_Detection_Data/'
 for home, dirs, files in os.walk(datapath+'deceptive_neg'):
     for filename in files:
         sources[0].append(home+'/'+filename)
@@ -76,11 +78,10 @@ labels=encoder.fit_transform(labels)
 
 #train-test split
 train_txt, valid_txt, train_LIWC, valid_LIWC, train_labels, valid_labels= model_selection.train_test_split(text, dfLIWC, labels, test_size = 0.10, random_state = 0)
-max_acc=0.0
-max_f=100
+
 # for i in range(1690,1695,1):
 i=1692
-print("\n\nMax features = %d"%i)
+print("Max features = %d\n"%i)
 tfidf_vect_ngram = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', ngram_range=(1,2), lowercase=True, stop_words=stopwords, max_features=i)
 tfidf_vect_ngram.fit(train_txt)
 xtrain_tfidf_ngram =  tfidf_vect_ngram.transform(train_txt)
@@ -88,15 +89,22 @@ xvalid_tfidf_ngram =  tfidf_vect_ngram.transform(valid_txt)
 
 train_X=np.empty((len(train_txt),i+93))
 # print(xtrain_tfidf_ngram.todense().shape)
-# print(train_LIWC.shape)
-# print(np.concatenate((xtrain_tfidf_ngram.todense(),train_LIWC),axis=1).shape)
 np.concatenate((xtrain_tfidf_ngram.todense(),train_LIWC),axis=1,out=train_X)
-# print(train_X)
 
 valid_X=np.empty((len(valid_txt),i+93))
-# print(xvalid_tfidf_ngram.todense().shape)
-# print(valid_LIWC.shape)
 np.concatenate((xvalid_tfidf_ngram.todense(),valid_LIWC),axis=1,out=valid_X)
+
+clf=svm.SVC(kernel='linear')
+
+# K-fold cross validation to test model and data variance
+# k=10
+# print("Performing %d fold cross validation..."%k)
+# score = cross_validate(clf, train_X, train_labels, cv=k, scoring=['accuracy','f1_macro'])
+# # print("SVM Accuracy = %.3f +- %.3f"%(score2.mean(),score2.std()*2))
+# print("Results:\nAccuracy:",score['test_accuracy'],"\nF1 score:",score['test_f1_macro'])
+# avgF1=mean(score['test_f1_macro'])
+# avgAcc=mean(score['test_accuracy'])
+# print("Overall F1 = %.3f\nAccuracy = %.3f\n"%(avgF1,avgAcc))
 
 def train_model(classifier, feature_vector_train, train_label, feature_vector_valid, valid_label):
         # fit the training dataset on the classifier
@@ -105,13 +113,9 @@ def train_model(classifier, feature_vector_train, train_label, feature_vector_va
         # predict the labels on validation dataset
         predictions = classifier.predict(feature_vector_valid)
 
-        return accuracy_score(predictions, valid_label)
+        return {'accuracy':accuracy_score(predictions, valid_label),'f1':f1_score(predictions, valid_label)}
 
 # SVM on Ngram Level TF IDF Vectors
-print("Classifying using SVM...")
-accuracy = train_model(svm.SVC(kernel='linear'), train_X, train_labels, valid_X, valid_labels)
-print("SVM, N-Gram Vectors: ", accuracy)
-# if(accuracy>max_acc):
-#         max_acc=accuracy
-#         max_f=i
-# print("Max Accuracy = %.3f\n Max features = %d"%(max_acc,max_f))
+print("Classifying test data using SVM...")
+result = train_model(clf, train_X, train_labels, valid_X, valid_labels)
+print("Accuracy score = %.3f\nF1 score = %.3f"%(result['accuracy'],result['f1']))
